9c5c0b267b3783f78d6cdf4d9e8382c4cbf9b5ff
shahinsahebi <shahin@gmail.com>
2021-06-04 16:24:39 +0430

    adding backtest plot

--- backtest.py
+++ backtest.py
@@ -13,22 +13,24 @@ import datetime
 from btexchange import Btexchange
 class Backtest:
     def __init__(self):
-        self.pair="XRPUSDT"
+        self.pairs=["ADAUSDT","ETHUSDT", "BTCUSDT"]
         self.period = 1 #period in months
-        self.start_date = datetime.datetime(2020, 9, 1)
+        self.start_date = datetime.datetime(2020, 7, 1)
         self.market_type = "FUTURES" # FUTURES or SPOT
 
         self.initial_capital = 100 # in USDT
         self.portion_per_trade = 1 # 1 for all 0.2 for 20 percent
         self.candles_length = 20 # candles strategy needs to decide
         self.ccstra = rsistochstra.Strategy()
-        self.ex1 =Btexchange()
+        self.pl = Plot()
 
-    def start(self):
+
+    def start(self, pair):
+        ex1 =Btexchange()
         print(datetime.datetime.now())
         print(str(self.start_date))
         self.start_timespan = int(datetime.datetime.timestamp(self.start_date)*1000) - (self.candles_length * 5 * 60000)
-        self.finish_timespan = self.start_timespan+(self.period * 30 * 24 * 12 * 5 * 60000)
+        self.finish_timespan = self.start_timespan+(self.period * 2 * 24 * 12 * 5 * 60000)
         print(self.start_timespan)
         self.conn = None
         if(self.market_type=="FUTURES"):
@@ -41,7 +43,7 @@ class Backtest:
             print(e)
         print("loading data ...")
         self.cur = self.conn.cursor()
-        self.cur.execute("SELECT timespan1, CAST(open as float) as open ,CAST(high as float) as high ,CAST(low as float) as low ,CAST(close as float) as close,volume FROM "+self.pair+" where timespan1>"+str(self.start_timespan-1)+" and timespan1<"+str(self.finish_timespan+1)+"")
+        self.cur.execute("SELECT timespan1, CAST(open as float) as open ,CAST(high as float) as high ,CAST(low as float) as low ,CAST(close as float) as close,volume FROM "+pair+" where timespan1>"+str(self.start_timespan-1)+" and timespan1<"+str(self.finish_timespan+1)+"")
 
         self.rows = self.cur.fetchall()
         print("data loaded ...")
@@ -55,14 +57,13 @@ class Backtest:
             #call
             sub_df = df.iloc[ind:ind+20, ]
             sub_df = sub_df.reindex(index=sub_df.index[::1])
-            ccstra.Strategy().exec(sym=self.pair,data5min=sub_df,ex1=self.ex1)
+            ccstra.Strategy().exec(sym=pair,data5min=sub_df,ex1=ex1)
             if((ind+22)>total_rows):
                 self.go = False
             else:
                 ind += 1
         print(datetime.datetime.now())
-        pl = Plot()
-        pl.plot(self.ex1.closed_positions)
+        self.pl.plot(ex1.closed_positions, pair, self.start_date)
 
         # print(self.rows)
         # pair = input("Enter pair (ex:'btcusdt' or 'all' for all pairs): ")
@@ -72,5 +73,14 @@ class Backtest:
         #
         # print(pair)
 
+    def main(self):
+            for p in self.pairs:
+                self.start(p)
+            self.pl.export()
+
+
+
+
+
 bt=Backtest()
-bt.start()
+bt.main()
--- plot.py
+++ plot.py
@@ -2,8 +2,9 @@ import plotly
 import plotly.graph_objects as go
 from btposition import Btposition
 import os
-from datetime import date
+from datetime import date, datetime
 import numpy as np
+import matplotlib, random
 
 
 class Plot:
@@ -11,8 +12,18 @@ class Plot:
     def __init__(self):
         if not os.path.exists('backtest'):
             os.makedirs('backtest')
+        self.fig = go.Figure()
 
-    def plot(self, positions: Btposition):
+
+
+    def export(self):
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H::%M::%S")
+        plotly.offline.plot(self.fig, filename='backtest\{}.html'.format(dt_string), auto_open=False)
+
+
+
+    def plot(self, positions: Btposition, symbol, start_date):
 
         # return self.timestamp + "," + \
         #     self.symbol + "," + \
@@ -33,25 +44,35 @@ class Plot:
         currBalance = 100
 
         for pos in positions:
-            timestamp.append(pos.timestamp)
-
+            ts = datetime.fromtimestamp(pos.timestamp/1000)
+            print(ts.strftime('%Y-%m-%d %H:%M:%S'))
+            timestamp.append(ts)
             currBalance = float(currBalance * ((100 + pos.unrealizedProfit) / 100))
             balance.append(currBalance)
 
-        fig = go.Figure()
         # Create and style traces
-        fig.add_trace(go.Scatter(x=timestamp, y=balance, name='Balance',
-                                 line=dict(color='royalblue', width=4)))
+        self.fig.add_trace(go.Scatter(x=timestamp, y=balance, name=symbol,
+                                 line=dict(color=self.random_color(), width=4)))
 
         # Edit the layout
-        fig.update_layout(title='Balance',
+        self.fig.update_layout(title='Balance',
                           xaxis_title='Time',
                           yaxis_title='Balance')
 
-        fig.show()
-
         # fig.show()
-        plotly.offline.plot(fig, filename='{}-{}.html'.format(), auto_open=False)
+
+    def random_color(self):
+
+        hex_colors_dic = {}
+        rgb_colors_dic = {}
+        hex_colors_only = []
+        for name, hex in matplotlib.colors.cnames.items():
+            hex_colors_only.append(hex)
+            hex_colors_dic[name] = hex
+            rgb_colors_dic[name] = matplotlib.colors.to_rgb(hex)
+
+        return random.choice(hex_colors_only)
+
 
 
 def main():
