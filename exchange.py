6e2f92b1ea97ffd8d715fa99a0afa1c7c013b2eb
shahinsahebi <shahin@gmail.com>
2021-07-27 01:53:32 +0430

    adding pair balance

--- exchange.py
+++ exchange.py
@@ -39,7 +39,7 @@ class Exchange:
         # self.log = logger
         self.leverage = 10
         self.order_size = 20
-        self.allowed_balance = 300
+        self.allowed_balance = 250
         self.stop_loss_perc = 1
 
 
