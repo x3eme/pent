ec7cf280ed2b1828c05a4209ae4f08ff8beb0b1f
shahinsahebi <shahin@gmail.com>
2021-09-05 02:19:48 +0430

    adding fix data

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ class main:
             # self.symbol_records = self.my_data.get_symbols()
 
             self.symbol_records = ["AAVEUSDT", "BNBUSDT", "DOGEUSDT", "FTMUSDT",
-                                   "ICPUSDT", "LITUSDT", "MATICUSDT", "OMGUSDT", "RUNEUSDT",
-                                   "SNXUSDT", "ADAUSDT", "ATOMUSDT", "BAKEUSDT", "DENTUSDT",
-                                   "GRTUSDT", "NEARUSDT", "NKNUSDT", "THETAUSDT", "LUNAUSDT", "XRPUSDT"]
+                                   "ICPUSDT", "LITUSDT", "MATICUSDT", "OMGUSDT", "COTIUSDT",
+                                   "ADAUSDT", "ATOMUSDT", "BAKEUSDT", "DENTUSDT",
+                                   "AVAXUSDT", "NEARUSDT", "NKNUSDT", "THETAUSDT", "LUNAUSDT", "XRPUSDT"]
             # self.symbol_records = ["AAVEUSDt"]
             self.c3 = candles3.Strategy(self.symbol_records)
             cnt = len(self.symbol_records)
